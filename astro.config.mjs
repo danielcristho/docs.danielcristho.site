bb7720c7c760b129f915538a60a082e8e0d8f4af
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-10-07 19:12:08 +0700

    feat: update config, add theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,48 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import react from '@astrojs/react';
+import react from '@astrojs/react'; // Import React
+import starlightThemeFlexoki from 'starlight-theme-flexoki'
 
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: "Daniel's Documentation",
-        social: {
-            github: 'https://github.com/danielcristho',
-        },
-        sidebar: [
-            { label: 'Intro', slug: 'index' },
-            {
-                label: 'Docker',
-                autogenerate: { directory: 'docker' },
+    integrations: [
+        starlightThemeFlexoki({
+            accentColor: "magenta",
+        }),
+
+        starlight({
+            title: "danielcristho's docs",
+            social: {
+                github: 'https://github.com/danielcristho',
             },
-            {
-                label: 'Kubernetes',
-                autogenerate: { directory: 'kubernetes' },
-            },
-            {
-                label: 'DevOps & Automation',
-                autogenerate: { directory: 'devops-and-automation' },
-            },
-            {
-                label: 'Linux',
-                autogenerate: { directory: 'linux' },
-            },
-            {
-                label: 'Terminal',
-                autogenerate: { directory: 'terminal' },
-            }
-        ],
-        customCss: ["./src/styles/custom.css"]
-		}), react()],
+            sidebar: [
+                { label: 'Intro', slug: 'index' },
+                {
+                    label: 'Docker',
+                    autogenerate: { directory: 'docker' },
+                },
+                {
+                    label: 'Kubernetes',
+                    autogenerate: { directory: 'kubernetes' },
+                },
+                {
+                    label: 'DevOps & Automation',
+                    autogenerate: { directory: 'devops-and-automation' },
+                },
+                {
+                    label: 'Linux',
+                    autogenerate: { directory: 'linux' },
+                },
+                {
+                    label: 'Terminal',
+                    autogenerate: { directory: 'terminal' },
+                }
+            ],
+            customCss: ["./src/styles/custom.css"]
+        }), 
+        
+        react()
+    ],
 });
